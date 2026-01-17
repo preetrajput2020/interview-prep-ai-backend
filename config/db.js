daa0b415696e9175bbeb8cdadbfc883ba02f5d06
Preet Rajput <145763043+preetrajput2020@users.noreply.github.com>
2026-01-17 10:07:56 +0530

    add gitignore

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {});
-        console.log("MongoDB connected");
+        await mongoose.connect(process.env.MO_URI, {});
+        console.log("MongoDB connected");NGO
     } catch (err) {
         console.error("Error connecting to MongoDB", err);
         process.exit(1);
